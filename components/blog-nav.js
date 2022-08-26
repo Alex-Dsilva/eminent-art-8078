4d32fae8d60855924f56d93d97f59be20d0bf5e9
yogeshnishad007 <yogiatoy27518@gmail.com>
2022-08-26 20:35:17 +0530

    day3 new update

---
+++ components/blog-nav.js
@@ -0,0 +1,85 @@
+
+
+  function nav(){
+
+
+    return ` <div id="navbar">
+
+    <button><a href="blog.html">BLOG </a></button>
+   <button><a href="product.html">PRODUCTIVITY </a></button> 
+   <button><a href="remote.html">REMOTE WORK </a></button>
+   <button><a href="best.html">BEST PRACTICES </a></button>
+   <button><a href="resource.html">RESOURCES</a></button>
+
+ </div>
+
+
+   <div id="timeCard">
+
+    <p>BEST PRACTICES
+        ARTICLES WITH TIPS AND RECOMMENDATIONS OF BEST PRACTICES AT WORK.</p>
+
+        <img src="https://trackingtime.co/wp-content/uploads/2022/08/Ilustracio%CC%81n-Time-Off_SVG-240x146px.svg" alt="">
+
+        <h2>Attendance & Timecards v2</h2>
+
+        <a href="#">READ ARTICLE</a>
+
+   </div>
+
+
+   <div id="articl-div">
+    <div id="div2">
+          <img src="https://trackingtime.co/wp-content/uploads/2022/08/Remote-Work-Productivity-Tricks-and-Tools.svg" alt="">
+          <h4>The Remote Work Productivity Tricks and Tools You Need to Know</h4>
+
+    </div>
+
+    <div >
+         <img src="https://trackingtime.co/wp-content/uploads/2022/04/Online-Communities.svg" alt="">
+         <h4>Work Productivity Best Practices for Your Remote Team</h4>
+        </div>
+
+    <div >
+        <img src="https://trackingtime.co/wp-content/uploads/2022/04/Positive-Company-Culture-with-a-Remote-Team.svg" alt="">
+        <h4>7 Ways to Build a Positive Company Culture with a Remote Team</h4>
+    </div>
+
+    <div >
+        <img src="https://trackingtime.co/wp-content/uploads/2022/04/hybrid-work-model.svg" alt="">
+        <h4>How to Track the Performance of Your Remote Team</h4>
+    </div>
+
+    <div >
+        <img src="https://trackingtime.co/wp-content/uploads/2022/02/Smart-collaboration-1.svg" alt="">
+        <h4>Hybrid Work: How to Collaborate Smartly</h4>
+    </div>
+
+    <div>
+        <img src="https://trackingtime.co/wp-content/uploads/2022/01/Remote-work-productivity.svg" alt="">
+        <h4>7 Remote Work Productivity Tips to Motivate Your Teams</h4>
+    </div>
+
+
+    <div >
+        <img src="https://trackingtime.co/wp-content/uploads/2021/12/5-communication-tools.svg" alt="">
+        <h4>5 Communication Tools You Need for Remote Work</h4>
+    </div>
+
+    <div >
+        <img src="https://trackingtime.co/wp-content/uploads/2021/12/freelancing-productivity-and-budget-problems-1.svg" alt="">
+        <h4>How to Improve Employee Productivity While Working Remotely?</h4>
+    </div>
+
+    <div >
+        <img src="https://trackingtime.co/wp-content/uploads/2021/11/Time-saving-techniques.svg" alt="">
+       <h4>5 Alternative Time-Saving Techniques to Boost Productivity</h4>
+    </div>
+
+          
+   </div>
+
+`
+}
+
+export default nav;
---
+++ js/function.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+let pro=()=>{
+
+    window.location.href="product-1.html"
+    console.log("yes")
+}
+
+document.getElementById("div-2").addEventListener("click",pro)
